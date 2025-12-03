ad165633e7a3371f6a6d87722a15756501e49e9f
NinjaFromConsole <rothtyler24@gmail.com>
2025-12-03 14:33:54 -0800

    Improved Map + Zoom
    - Very much improved the europe map image by overlaying two maps
    - Added camera zooming using the scroll wheel

--- delugeProject/Assets/Scripts/CameraControls.cs
+++ delugeProject/Assets/Scripts/CameraControls.cs
@@ -6,19 +6,27 @@ public class CameraControls : MonoBehaviour
     [SerializeField] private Vector2 topRightCorner;
     [SerializeField] private Vector2 bottomLeftCorner;
     [SerializeField] private float dragModifier = 0.7f;
+    [SerializeField] private float scrollModifier = 0.7f;
+    [SerializeField] private float minZoom = 0.5f;
+    [SerializeField] private float maxZoom = 9f;
 
     private Vector2 lastMousePosition;
     private InputAction mousePosition;
     private InputAction mouseDown;
+    private InputAction mouseScroll;
+    private Camera cameraComponent;
 
 	private void Start()
 	{
 		mousePosition = InputSystem.actions.FindAction("MousePosition");
 		mouseDown = InputSystem.actions.FindAction("MouseDown");
+		mouseScroll = InputSystem.actions.FindAction("MouseScroll");
+		cameraComponent = gameObject.GetComponent<Camera>();
 	}
 
 	private void Update()
     {
+        //Camera Dragging
         if (mouseDown.WasPressedThisFrame())
 		{
 			lastMousePosition = mousePosition.ReadValue<Vector2>();
@@ -30,10 +38,33 @@ public class CameraControls : MonoBehaviour
             lastMousePosition = newMousePosition;
 		}
 
-        transform.position = new Vector3(
-            Mathf.Max(bottomLeftCorner.x, Mathf.Min(topRightCorner.x, transform.position.x)),
-            Mathf.Max(bottomLeftCorner.y, Mathf.Min(topRightCorner.y, transform.position.y)),
-            transform.position.z
-        );
+        //Camera Zooming
+        if (mouseScroll.ReadValue<Vector2>() != Vector2.zero)
+		{
+			cameraComponent.orthographicSize -= mouseScroll.ReadValue<Vector2>().y * scrollModifier;
+            cameraComponent.orthographicSize = Mathf.Max(minZoom, Mathf.Min(maxZoom, cameraComponent.orthographicSize));
+		}
+
+        //Camera bounds
+        Vector3 cameraBottomLeftCorner = cameraComponent.ViewportToWorldPoint(new Vector3(0, 0, cameraComponent.nearClipPlane));
+        Vector3 cameraTopRightCorner = cameraComponent.ViewportToWorldPoint(new Vector3(1, 1, cameraComponent.nearClipPlane));
+
+        if (cameraBottomLeftCorner.x < bottomLeftCorner.x)
+		{
+			transform.position = new Vector3(transform.position.x + bottomLeftCorner.x - cameraBottomLeftCorner.x, transform.position.y, transform.position.z);
+		}
+        else if (cameraTopRightCorner.x > topRightCorner.x)
+		{
+			transform.position = new Vector3(transform.position.x + topRightCorner.x - cameraTopRightCorner.x, transform.position.y, transform.position.z);
+		}
+
+        if (cameraBottomLeftCorner.y < bottomLeftCorner.y)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y + bottomLeftCorner.y - cameraBottomLeftCorner.y, transform.position.z);
+		}
+        else if (cameraTopRightCorner.y > topRightCorner.y)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y + topRightCorner.y - cameraTopRightCorner.y, transform.position.z);
+		}
     }
 }
