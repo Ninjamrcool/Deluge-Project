b107f568e0ac52b271a690631a6c09573314e84a
NinjaFromConsole <rothtyler24@gmail.com>
2025-12-07 20:00:54 -0800

    Overlay Morphing
    - Overlays now gradually change shape to their new position when moving the timeline
    (Note: Might need a smarter way to pick midpoints later on if the morphing looks bad for actual borders)

--- delugeProject/Assets/Scripts/OverlayManagers.cs
+++ delugeProject/Assets/Scripts/OverlayManagers.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.U2D;
+using System.Collections;
 
 public class OverlayManagers : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class OverlayManagers : MonoBehaviour
 
     [SerializeField] private OverlayYear[] overlayYears;
     [SerializeField] private Timeline timelineScript;
+    [SerializeField] private SpriteShapeController mainSpriteShape;
+    [SerializeField] private SpriteShapeRenderer mainSpriteRenderer;
+    [SerializeField] private float morphTime;
+    [SerializeField] private float maxAlpha;
 
     private void Start()
     {
@@ -25,12 +30,99 @@ public class OverlayManagers : MonoBehaviour
 		{
 			if (overlayYears[i].year <= year && (i == overlayYears.Length - 1 || overlayYears[i+1].year > year))
 			{
-				overlayYears[i].spriteShapeGameObject.GetComponent<SpriteShapeRenderer>().enabled = true;
-			}
-			else
-			{
-                overlayYears[i].spriteShapeGameObject.GetComponent<SpriteShapeRenderer>().enabled = false;
+                StopAllCoroutines();
+                StartCoroutine(MorphMainSpriteShape(overlayYears[i].spriteShapeGameObject.GetComponent<SpriteShapeController>().spline));
+                StartCoroutine(FadeRendererTo(maxAlpha));
+                return;
 			}
 		}
+        StartCoroutine(FadeRendererTo(0f));
     }
+
+    private IEnumerator MorphMainSpriteShape(Spline morphInto)
+	{
+        Spline morphIntoCopy = new Spline();
+        CopySplineData(morphIntoCopy, morphInto);
+
+        //make sure splines have same amount of points
+        AddSplinePointsToTargetAmount(mainSpriteShape.spline, morphIntoCopy.GetPointCount());
+        AddSplinePointsToTargetAmount(morphIntoCopy, mainSpriteShape.spline.GetPointCount());
+
+        Spline originalSpline = new Spline();
+        CopySplineData(originalSpline, mainSpriteShape.spline);
+        
+        float time = 0f;
+        while(time < morphTime)
+		{
+            for (int i = 0; i < morphIntoCopy.GetPointCount(); i++)
+			{
+				Vector3 finalPosition = morphIntoCopy.GetPosition(i);
+                Vector3 originalPosition = originalSpline.GetPosition(i);
+
+				try
+				{
+                    mainSpriteShape.spline.SetPosition(i, Vector3.Lerp(originalPosition, finalPosition, time/morphTime));
+				}
+				catch
+				{
+					//Points too close
+                    mainSpriteShape.spline.RemovePointAt(i);
+                    morphIntoCopy.RemovePointAt(i); //removing this point might cause visual bugs, but itl fix after
+                    i -= 1;
+                    //Debug.Log("removed point: " + i);
+				}
+			}
+
+            mainSpriteShape.RefreshSpriteShape();
+			time += Time.deltaTime;
+            yield return null;
+		}
+
+
+        CopySplineData(mainSpriteShape.spline, morphInto);
+        mainSpriteShape.RefreshSpriteShape();
+	}
+
+
+    private void CopySplineData(Spline copyInto, Spline copyFrom){
+        copyInto.Clear();
+
+        for (int i = 0; i < copyFrom.GetPointCount(); i++)
+		{
+			copyInto.InsertPointAt(i, copyFrom.GetPosition(i));
+            copyInto.SetTangentMode(i, ShapeTangentMode.Linear);
+		}
+    }
+
+    private void AddSplinePointsToTargetAmount(Spline spline, int numPoints)
+	{
+		while (spline.GetPointCount() < numPoints)
+		{
+            try{
+                int randomIndex = Random.Range(0, spline.GetPointCount() - 1);
+                Vector3 midpoint = Vector3.Lerp(spline.GetPosition(randomIndex), spline.GetPosition(randomIndex + 1), 0.5f);
+			    spline.InsertPointAt(randomIndex + 1, midpoint);
+            }
+			catch
+			{
+                //Points too close
+                //Debug.Log("failed to add point");
+				continue;
+			}
+		}
+	}
+
+    private IEnumerator FadeRendererTo(float alpha)
+	{
+        float time = 0f;
+        float originalAlpha = mainSpriteRenderer.color.a;
+        while(time < morphTime)
+		{
+            mainSpriteRenderer.color = new Color(mainSpriteRenderer.color.r, mainSpriteRenderer.color.g, mainSpriteRenderer.color.b, Mathf.SmoothStep(originalAlpha, alpha, time/morphTime));
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        mainSpriteRenderer.color = new Color(mainSpriteRenderer.color.r, mainSpriteRenderer.color.g, mainSpriteRenderer.color.b, Mathf.SmoothStep(originalAlpha, alpha, time/morphTime));
+	}
 }
